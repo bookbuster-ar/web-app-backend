8e9618ea8bba340e1b2a808d33bd1d5bde3b3272
David <jefferson-education@outlook.com>
2023-08-01 12:01:35 -0500

    Configuración del servidor

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const router = require('./routes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.use('/', router);
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not found' });
+});
+
+module.exports = app;
