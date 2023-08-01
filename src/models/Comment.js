7efe0fe6c0fd783aecb81abd132a23a1c7db0b7a
Carolina Palacios <caaro.palacios@hotmail.com>
2023-08-01 16:50:53 -0300

    modelo comment

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,23 +1,20 @@
 const sequelize = require('../config/database');
 const { DataTypes } = require('sequelize');
 
-const Review = sequelize.define(
-  'review',
+const Comment = sequelize.define(
+  'comment',
   {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     content: {
       type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    rating: {
-      type: DataTypes.INTEGER,
       allowNull: false,
     },
   },
   { timestamps: false, freezeTableName: true }
 );
 
-module.exports = Review;
+module.exports = Comment;
