04732aee4b9479e96e6784861685820987d21a11
Carolina Palacios <caaro.palacios@hotmail.com>
2023-08-01 16:45:04 -0300

    modelo review

---
+++ src/models/reviewModel.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/database');
+const { DataTypes } = require('sequelize');
+
+const Review = sequelize.define(
+  'review',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  { timestamps: false, freezeTableName: true }
+);
+
+module.exports = Review;
