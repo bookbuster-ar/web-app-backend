9a9167975436ba0fa9928846ccab8107a3f0f657
David <jefferson-education@outlook.com>
2023-08-05 20:26:05 -0500

    Limpieza de carga de datos a DB

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const app = require('./src/app');
 const sequelize = require('./src/config/database');
 
 const data = require('./src/utils/data');
-const loadData = require('./src/utils/loadData');
+// const loadData = require('./src/utils/loadData');
 
 require('./src/models');
 require('./src/models/associations');
 
 app.listen(3001, async () => {
   try {
-    await sequelize.sync({ force: true, logging: false });
-    loadData(data);
+    await sequelize.sync({ alter: true, logging: false });
+    // loadData(data);
   } catch (error) {
     console.log(error.message);
   }
