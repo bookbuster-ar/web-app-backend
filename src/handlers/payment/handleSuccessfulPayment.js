e1e4d12b3a81e6b9915fe5845c2568c3d20bd248
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-08-09 16:57:46 -0300

    handler successfulPayment y controller

--- src/handlers/payment/handleSuccessfulPayment.js
+++ src/handlers/payment/handleSuccessfulPayment.js
@@ -1,6 +1,14 @@
-const handleSuccessfulPayment = (req, res) => {
+const handleSuccessfulPayment = async(req, res) => {
+
+    try{
+      
     console.log(req.query);
+    const result = await registerOrder(req.query);
+
     res.send('pago con exito');
+    }catch(error){
+      res.status(500).json({error: error.message});
+    }
   };
   
   module.exports = handleSuccessfulPayment;
