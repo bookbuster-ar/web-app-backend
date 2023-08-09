b5c871fb59cccb90567ffb9c37974fb38dadd556
David <jefferson-education@outlook.com>
2023-08-09 14:17:58 -0500

    Corrección de catch en el registro de usuario

--- src/handlers/auth/handleRegisterLocalUser.js
+++ src/handlers/auth/handleRegisterLocalUser.js
@@ -15,10 +15,10 @@ const handleRegisterLocalUser = async (req, res) => {
     });
   } catch (error) {
     if (error.message === 'Ya existe un usuario con esa dirección de correo') {
-      return res.status(409).json({ error: 'Error de registro' });
+      return res.status(409).json({ error: error.message });
     }
     console.error(error);
-    return res.status(500).json({ error: 'Error interno del servidor' });
+    return res.status(500).json({ error: error.message });
   }
 };
 
