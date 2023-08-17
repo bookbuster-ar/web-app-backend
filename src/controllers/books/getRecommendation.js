50e56469dc840a1ba79f1c49c82c3fff0bcf333c
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-08-17 13:20:58 -0300

    1

--- src/controllers/books/getRecommendation.js
+++ src/controllers/books/getRecommendation.js
@@ -1,4 +1,55 @@
-const getRecommendation = async () => {
+const { Book, BookFormat, BookFormatInterm, PublishedBook } = require('../../models');
+
+const getRecommendation = async (format, pages, genres) => {
+    let whereCondition;
+
+
+    if(pages){ 
+    switch (pages) {
+        case "0to50pages":
+            whereCondition = { pages: { [Op.between]: [0, 50] } };
+            break;
+        case "50to100pages":
+            whereCondition = { pages: { [Op.between]: [50, 100] } };
+            break;
+        case "100to200pages":
+            whereCondition = { pages: { [Op.between]: [100, 200] } };
+            break;
+        case "Mayora200pages":
+            whereCondition = { pages: { [Op.gt]: 200 } };
+            break;
+        default:
+            throw new Error("Rango invalido");
+    }
+  }
+
+  if(format){
+    const nameFormat = await BookFormat.findOne({ where: { name: format } });
+
+  if (!nameFormat) {
+    throw new Error('Formato no encontrado');
+  }
+
+  const booksFormatInterm = await BookFormatInterm.findAll({
+    where:{book_format_id: nameFormat.id},
+    include: [
+      {
+        model: Book,
+        as: 'book',
+        include: [ 'images',
+        {
+            model: PublishedBook,
+            as: 'published_book',
+          },
+        ],
+      },
+    ],
+  });
+  }
+
+  if(genres){
+      
+  }
     
 }
 
--- src/handlers/books/handleRecommendation.js
+++ src/handlers/books/handleRecommendation.js
@@ -1,11 +1,15 @@
 const { getRecommendation } = require('../../controllers');
 const handleRecommendation = async (req, res) => {
     try {
-        
+        const format = req.query.format;
+        const pages = req.query.pages;
+        const genres = req.query.genres;        
 
-        const recommendation = await getRecommendation();
+        const recommendation = await getRecommendation(format, pages, genres);
+
+        return res.status(200).json(recommendation);
     } catch (error) {
-        
+        return res.status(500).json({ error: error.message });
     }
 }
 
