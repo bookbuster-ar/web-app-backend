5b5c7b48dbd273354db0b17c78da1739a6827802
Carolina Palacios <caaro.palacios@hotmail.com>
2023-08-17 17:26:47 -0300

    borro console.log

--- src/controllers/books/getFilteredBooks.js
+++ src/controllers/books/getFilteredBooks.js
@@ -62,7 +62,6 @@ const getFilteredBooks = async (req, { title, author, search }) => {
 
     const filteredData = filteredBooks.map((field) => {
       const [cover, ...extra] = field.book?.images.map((image) => image.image);
-      console.log(field);
       return {
         id: field.book.id,
         images: { cover: cover ?? null },
