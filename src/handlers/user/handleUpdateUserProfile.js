8a8e489330e92dbe6501b458f07cf5ec90ebcd9d
David <jefferson-education@outlook.com>
2023-08-17 10:52:57 -0500

    Guardado de handleUpdateUserProfile

--- src/handlers/user/handleUpdateUserProfile.js
+++ src/handlers/user/handleUpdateUserProfile.js
@@ -1,3 +1,5 @@
+const { updateUserProfile } = require('../../controllers');
+
 const handleUpdateUserProfile = async (req, res) => {
   const { userid: userId } = req.headers;
   const {
@@ -12,6 +14,23 @@ const handleUpdateUserProfile = async (req, res) => {
     province,
     postalCode,
   } = req.body;
+
+  const userInfo = {
+    id: userId,
+    name,
+    lastname,
+    email,
+    aboutMe,
+    wantNotifications,
+  };
+  const userAddress = { country, address, city, province, postalCode };
+
+  try {
+    const updatedUser = await updateUserProfile(userInfo, userAddress);
+    return res.status(201).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = handleUpdateUserProfile;
