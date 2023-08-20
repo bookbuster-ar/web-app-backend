e0cfce6c0784540f45403f983a853bdb2370b1a1
David <jefferson-education@outlook.com>
2023-08-20 17:00:41 -0500

    Config de Cors

--- src/app.js
+++ src/app.js
@@ -9,9 +9,14 @@ const router = require('./routes');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(express.json());
-
 
 app.use('/api', router);
 
