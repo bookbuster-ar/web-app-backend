785bf142353265838b076d34e96b8b7e06603519
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-08-21 13:51:51 -0300

    Lista ruta de editar nombre con otro mensaje

--- src/handlers/shelves/handleEditNameShelf.js
+++ src/handlers/shelves/handleEditNameShelf.js
@@ -5,7 +5,11 @@ const handleEditNameShelf= async (req, res) => {
         const name = req.query.name;
         const shelfId = req.query.shelfId;
         const editName = await editNameShelf(name , shelfId);
-        return res.status(200).json(editName);
+        
+        if(editName[0] === 1){
+            return res.status(200).send('Nombre de estante editado exitosamente');
+        }
+    
     } catch (error) {
         res.status(500).json({ error: error.message });
             
