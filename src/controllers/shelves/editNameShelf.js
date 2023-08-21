6fff7321738f176fca7ced0953503e3838a2d740
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-08-21 12:50:05 -0300

    Lista ruta para cambiarle el nombre a las estanterias

--- src/controllers/shelves/editNameShelf.js
+++ src/controllers/shelves/editNameShelf.js
@@ -1,14 +1,14 @@
 const {BookShelfCategory} = require('../../models');
 
 const editNameShelf = async (name, shelfId) => {
-
-    const shelf = await BookShelfCategory.update({
-        where: {
-            id: shelfId
-        },
-        name: name
-    })
-
+    const shelf = await BookShelfCategory.update(
+        { name: name },
+        { 
+            where: {
+                id: shelfId
+            } 
+        } 
+    );
     return shelf;
 }
 
--- src/handlers/shelves/handleEditNameShelf.js
+++ src/handlers/shelves/handleEditNameShelf.js
@@ -2,7 +2,7 @@ const { editNameShelf } = require('../../controllers');
 
 const handleEditNameShelf= async (req, res) => {
     try {
-        const { name } = req.query;
+        const name = req.query.name;
         const shelfId = req.query.shelfId;
         const editName = await editNameShelf(name , shelfId);
         return res.status(200).json(editName);
