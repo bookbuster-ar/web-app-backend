b5b4ae76d1fac1215847b7b43a0ed648c3b2b483
Carolina Palacios <caaro.palacios@hotmail.com>
2023-08-22 15:21:10 -0300

    correccion de importacion de handler

--- src/routes/shelves/index.js
+++ src/routes/shelves/index.js
@@ -1,7 +1,16 @@
 const { Router } = require('express');
 const shelvesRouter = Router();
 
-const { handleGetShelves , handleGetShelfWithBooks, handleAddBookToShelf , handleDeleteBookFromShelf , handleGetBooksOffTheShelf , handleCreateNewShelf , handleDeleteNewShelf , handleEditNameShelf} = require('../../handlers');
+const {
+  handleGetShelves,
+  handleGetShelfWithBooks,
+  handleAddBookToShelf,
+  handleDeleteBookFromShelf,
+  handleGetBooksOfTheShelf,
+  handleCreateNewShelf,
+  handleDeleteNewShelf,
+  handleEditNameShelf,
+} = require('../../handlers');
 const verifySession = require('../../middlewares/verifySession');
 
 shelvesRouter.get('/', verifySession, handleGetShelves);
@@ -10,7 +19,11 @@ shelvesRouter.post('/addBookToShelf', verifySession, handleAddBookToShelf);
 shelvesRouter.post('/createNewShelf', verifySession, handleCreateNewShelf);
 shelvesRouter.put('/editNameShelf', verifySession, handleEditNameShelf);
 shelvesRouter.delete('/deleteShelf', verifySession, handleDeleteNewShelf);
-shelvesRouter.delete('/deleteBookFromShelf', verifySession, handleDeleteBookFromShelf);
-shelvesRouter.get('/shelfWithBooks',handleGetShelfWithBooks )
+shelvesRouter.delete(
+  '/deleteBookFromShelf',
+  verifySession,
+  handleDeleteBookFromShelf
+);
+shelvesRouter.get('/shelfWithBooks', handleGetShelfWithBooks);
 
 module.exports = shelvesRouter;
